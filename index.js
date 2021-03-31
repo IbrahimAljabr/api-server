6c40e25da916ce08d78382a79b9244751c6356ec
Ibrahim aljabr <ibrahim_i95@hotmail.com>
2021-04-01 00:57:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = require('./src/server.js');
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(process.env.MONGODB,{ useNewUrlParser: true ,useUnifiedTopology: true })
+  .connect(process.env.MONGODB_URL,{ useNewUrlParser: true ,useUnifiedTopology: true })
   .then(() => {
     server.start(process.env.PORT);
   })
