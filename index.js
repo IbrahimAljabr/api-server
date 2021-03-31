497da448072df978ae9ac133bc0fbd107d7683b0
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-04-01 01:15:51 +0300

    fix MONGODB_URI

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 const server = require('./src/server.js');
 const mongoose = require('mongoose');
 
+console.log(process.env.MONGODB_URI);
 mongoose
   .connect(process.env.MONGODB_URI,{ useNewUrlParser: true ,useUnifiedTopology: true })
   .then(() => {
