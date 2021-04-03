e92baec70be0e74ca6e5f5b38145908fe8b0d8cb
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2021-04-03 20:37:00 +0300

    heroku fix 8

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ require('dotenv').config();
 const server = require('./src/server.js');
 const mongoose = require('mongoose');
 
-// mongoose
-//   .connect(process.env.MONGODB_URI,{ useNewUrlParser: true ,useUnifiedTopology: true })
-//   .then(() => {
+mongoose
+  .connect(process.env.MONGODB_URI,{ useNewUrlParser: true ,useUnifiedTopology: true })
+  .then(() => {
     server.start(process.env.PORT);
-  // })
-  // .catch((e) => {
-  //   console.log('__CONNECTION ERROR__', e.message);
-  // });
+  })
+  .catch((e) => {
+    console.log('__CONNECTION ERROR__', e.message);
+  });
